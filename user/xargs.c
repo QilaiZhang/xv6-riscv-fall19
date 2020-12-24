4a08e601f18bea8c03b94c733639b619d0ffb7a6
QilaiZhang <180110118@stu.hit.edu.cn>
2020-12-24 14:50:43 +0800

    update

--- user/xargs.c
+++ user/xargs.c
@@ -14,9 +14,11 @@ int main(int argc, char* argv[]) {
     }
     // read arguments from stdin
     char argbuf[ARG_MAX_LENGTH];
+
     // arguments for subcommand.
     char *subargv[MAXARG];
     int subargc = 0;
+
     // read subargv from argv to get subcommand and arguments
     for (; subargc + 1 < argc; subargc++) {
         subargv[subargc] = argv[subargc + 1];
@@ -24,7 +26,9 @@ int main(int argc, char* argv[]) {
 
     int subargc_start = subargc;
     int runcmd = 0, argbuf_index = 0;
+
     while (read(0, argbuf + argbuf_index, sizeof(char)) == sizeof(char)) {
+        
         if (argbuf[argbuf_index] == '\n' || argbuf[argbuf_index] == ' ') {
             runcmd = (argbuf[argbuf_index] == '\n') ? 1 : 0;
             argbuf[argbuf_index] = 0;
